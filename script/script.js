66a2d1a41973a9294bf66ef3a1163bcbef407718
Bruno Eduardo do Nascimento Gomes <bengomes@emsurb.se.gov.br>
2024-10-15 12:28:38 -0300

    Alteracao no design / Corrigida a funcao de conversao

--- script/script.js
+++ script/script.js
@@ -1,13 +1,35 @@
+// Função que faz o calculo da transformacao celsius->fahrenheit
 function celsius_fahrenheit(celsius){
     const fahrenheit = (celsius * 9/5) + 32;
     return fahrenheit;
 }
 
-async function convertor() {
-    const graus_celsius = document.getElementById('celsius').value;
-    console.log(graus_celsius);
-    console.log(celsius_fahrenheit(graus_celsius));
+document.getElementById('celsius').value = ''; // Limpa o input de celsius
 
+// Função chamada pelo html para fazer a conversão do valor fornecido.
+function convertor() {
+    const graus_celsius = document.getElementById('celsius').value;
+    const graus_fahrenheit = document.getElementById('fahrenheit');  //Salva os valores da página html.
+
+    const fahrenheit_text = graus_fahrenheit.textContent; 
+    let fahrenheit_Num = Number(fahrenheit_text);       //Converte o valor da tag span para numérico.
+    fahrenheit_Num = celsius_fahrenheit(graus_celsius); //Atribui o valor da conversão para fahrenheit à variável.
+
+    if(graus_celsius == ''){     // Verifica se foi passada alguma entrada em graus celsius.
+        graus_fahrenheit.textContent = "TU É BURRO?";
+        document.getElementById('fieldset').style.marginBottom = 0; // Retira a margem do fieldset para aparecer o resultado.
+    }
+    else{
+        graus_fahrenheit.textContent = fahrenheit_Num + " °F";
+        document.getElementById('fieldset').style.marginBottom = 0;
+    }
     
-    document.getElementById('resultado').style.display = 'flex';
+    document.getElementById('resultado').style.display = 'flex';    // Mostra o resultado na tela.
 }
+
+// const graus_celsius = document.getElementById('celsius').value;
+// const graus_fahrenheit = document.getElementById('fahrenheit'); 
+
+// graus_celsius.addEventListener('HTMLInputElement', () => {
+//     convertor();
+// })
